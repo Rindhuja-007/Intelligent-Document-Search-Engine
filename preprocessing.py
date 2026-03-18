6142b3fd092119367471828c3465471160c12b48
Rindhuja <rindhujachandramohan@gmail.com>
2026-03-18 11:45:34 +0530

    render file alterd commit-5

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,22 @@
 import re
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+
+
+def _ensure_nltk_data():
+    try:
+        stopwords.words("english")
+    except LookupError:
+        nltk.download("stopwords", quiet=True)
+
+    try:
+        nltk.data.find("corpora/wordnet")
+    except LookupError:
+        nltk.download("wordnet", quiet=True)
+
+
+_ensure_nltk_data()
 
 stop_words = set(stopwords.words("english"))
 lemmatizer = WordNetLemmatizer()
